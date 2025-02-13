ee0ea9f48144b9e02a495b8743a8020d5a82aafa
Matthew Newport <48736345+MrNewport@users.noreply.github.com>
2025-02-13 14:33:54 -0700

    initial commit

---
+++ resources/js/Components/App.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="bg-blue-400 text-white p-4">
+        <h1 class="text-xl font-bold">
+            Hello Tailwind from Vue!
+        </h1>
+    </div>
+</template>
---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+// resources/js/app.js
+
+import { createApp } from 'vue';
+import App from './Components/App.vue';
+
+// If you're not relying on "input" via vite.config.js,
+// you might need to explicitly import the CSS here:
+// import '../css/app.css';
+
+createApp(App).mount('#app');
